b5ca9c111a2c61e9abb01784ebd78f0eed6eadfb
Rishabh-749 <jagtaprishabh074@gmail.com>
2026-05-24 23:30:14 +0530

    feat: initialize Express app with middleware, CORS configuration, and base API routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -17,7 +17,7 @@ app.use(cors({
 app.use("/api/auth", authProvider);
 app.use("/api/songs", songRoutes);
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "../public/index.html"));
 });
 
